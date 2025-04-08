8728daa2cf884021f202bbe4bf94d915cea106ef
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2025-04-08 09:38:19 +0200

    ...

--- Pregunta1.R
+++ Pregunta1.R
@@ -1,3 +1,63 @@
+#P(X=10) con lambda 25
+
+dpois(10, 25)
+
+#P(X=30) con lambda 25
+dpois(30, 25)
+
+#dibujar la poisson 
+
+x <- 0:50
+fx <- dpois(x, lambda = 25)
+fx
+
+plot(x, fx, col="red", pch=16)
+lines(x, fx, type="h", col="red")
+
+##P(X<=30)
+ppois(30, 25)
+
+##P(X>=30)
+1-ppois(29, 25)
+
+#dibujemos F(x)
+Fx <- ppois(x, 25)
+
+plot(x, Fx, type="s", col="red")
+
+####dibujar densidad exponencial
+
+t <- seq(0,0.15, 0.01)
+plot(t, dexp(t, 25), type="l")
+
+###probabilidad de esperar como mínimo 0.052
+#P(T>0.052)= 1- P(T<0.052)=1-F(0.052)
+1-pexp(0.052, 25)
+
+#simular experimento exponencial
+set.seed(88)
+simul <- rexp(500000, 25)
+
+hist(simul)
+
+mean(simul)
+
+#para exp E(T) =1/lamba
+##
+
+
+####problema 2
+
+1-pexp(13000, 1/10000)
+qexp(0.8, 1/10000)
+
+set.seed(85)
+simul <- rexp(100, 1/10000)
+mean(simul)
+median(simul)
+var(simul)
+
+###
 x <- 0:50
 fx <- dpois(x, 25)
 fx
